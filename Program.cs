e8764b1de251d55a894b267f5d75b11df994407f
Bradley Wilson <bradleywilson727@gmail.com>
2022-06-15 10:09:28 -0500

    completed assignment

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(100));
+
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(45));
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double temperature)
+        {
+            return ((temperature - 32) * 5 / 9);
+        }
+
+        public static double CelsiusToFahrenheit(double temperature)
+        {
+            return ((temperature * 9 / 5) + 32);
+        }
+    }
+}
